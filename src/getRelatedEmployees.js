aab041fdc354233a3eaf63d6d393b59f64018efd
Henrique Serres <henrique@gmail.com>
2021-11-24 20:23:06 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((gerentes) => gerentes.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const gerId = data.employees.filter((gerente) => gerente.managers.includes(managerId));
+    return gerId.map((el) => `${el.firstName} ${el.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
