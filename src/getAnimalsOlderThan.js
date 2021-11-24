88b92b7437b1ecea63034790c07a7d5ab857b8c5
Henrique Serres <henrique@gmail.com>
2021-11-24 16:24:19 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalFiltrado = species.filter((animais) => animais.name === animal);
+  console.log(animalFiltrado);
+  const ageAnimal = animalFiltrado[0].residents.every((el) => el.age >= age);
+  return ageAnimal;
 }
-
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
