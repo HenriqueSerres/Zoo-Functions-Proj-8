cce253f3c813eba3e9f46fb4923cdc9b24433619
Henrique Serres <henrique@gmail.com>
2021-11-25 15:47:46 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcId = data.employees.find((el) => el.id === id);
+  const firstAnim = funcId.responsibleFor.find((first) => first[0]);
+  const olderAnim = data.species.find((especie) => especie.id === firstAnim).residents
+    .sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = olderAnim;
+  return [name, sex, age];
 }
 
+console.log(getOldestFromFirstSpecies('c1f50212-35a6-4ecd-8223-f835538526c2'));
 module.exports = getOldestFromFirstSpecies;
